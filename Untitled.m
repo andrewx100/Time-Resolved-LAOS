a313097382defe6183459fc98fb7b62e194d0df4
Andrew Sun <andrewx100@gmail.com>
2011-09-03 22:03:27 +0800

    Untitled.m does the following things:  - reads from a text-based data file generated by FTR_Measruement  - determines the number of rows per cycle for coherent sampling  - coherently samples every 2 cycles along the time, does FFT, and obtained    relative higher harmonic amplitudes In/1 (n=3,5,7 and 9)  - generates and save data It is the core of the post-DAQ data processing tool.
    This commit is not ready for running.

---
+++ Untitled.m
@@ -0,0 +1,56 @@
+filename='D:\Dropbox\Data\Rheology\ARES\2011\06-29-2011\DTimeSwp (13) 000 ';
+x=load([filename '.txt'], '-ascii');
+[row,col]=size(x);
+
+% Signal and sampling parameters
+fs_prime=50;
+fi=1;
+
+
+% Add a time column
+t=0:1/fs_prime:(row-1)/fs_prime;
+t=t';
+x=[t x];
+
+% Total number of cycles
+Nr_of_Cycle=int32(t(row)/fi);
+
+% Rows per cycle
+row_per_cycle = round(fs_prime/fi);
+data=0;
+for i = 0:1:Nr_of_Cycle-3;
+    begin_row = round(i * fs_prime / fi)+1;
+    ti=x(begin_row,1);
+    data_i = x(begin_row:begin_row + 2 * row_per_cycle,:); % Take exactly 2 cycles
+    
+    [row_i, col_i]=size(data_i);
+    
+    freq_bin = 0:row_i-1; % vector of frequency bins
+    freq_res = fs_prime/row_i; % frequency resolution
+    freq_i = freq_bin * freq_res; % frequency axis
+    Xraw = fft(data_i(:,3))/row_i; % normalized fft
+    cutoff = ceil(row_i/2); % only use the first half of the FFT
+    freq_i=freq_i(1:cutoff);
+    Xraw=Xraw(1:cutoff);
+    
+    % frequency bins of the harmonics
+    f1 = round(fi / freq_res);
+    f3 = round(3 * fi / freq_res);
+    f5 = round(5 * fi / freq_res);
+    f7 = round(7 * fi / freq_res);
+    f9 = round(9 * fi / freq_res);
+    
+    % Relative intensity of harmonics
+    I31 = abs(Xraw(f3+1))/abs(Xraw(f1+1));
+    I51 = abs(Xraw(f5+1))/abs(Xraw(f1+1));
+    I71 = abs(Xraw(f7+1))/abs(Xraw(f1+1));
+    I91 = abs(Xraw(f9+1))/abs(Xraw(f1+1));
+    if i==0
+        data=[ti I31 I51 I71 I91];
+    else
+        data=[data;ti I31 I51 I71 I91];
+    end
+    
+end
+save([filename 'ft.txt'],'data','-ascii');
+
